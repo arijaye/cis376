875202a9d7c52cd81448b6d45d8ede62fd875f73
Ari <60124436+arijaye@users.noreply.github.com>
2023-02-06 08:49:03 -0500

    init Math lib

---
+++ Projects/1/Math/Matrix.py
@@ -0,0 +1,25 @@
+class Matrix:
+    def __init__(self, vectors):
+        self.vectors = vectors
+
+    def add(self, m1, m2):
+        # return matrix
+        return
+    
+    def subtract(self, m1, m2):
+        # return matrix
+        return
+    
+    def multiplyVM(self, v, m):
+        # return vector
+        return
+    
+    def multiplyMM(self, m1, m2):
+        # return matrix
+        return
+    
+    def equal(self, m1, m2):
+        # return boolean
+        return
+    
+    
---
+++ Projects/1/Math/Vector.py
@@ -0,0 +1,38 @@
+
+class Vector:
+
+    def __init__(self, a, b, c):
+        self.vector = (a,b,c)
+
+    def crossProduct(self, v1, v2):
+        # return vector
+        return
+    
+    def dotProduct(self, v1, v2):
+        # return scalar
+        return
+    
+    def angleBetween(self, v1, v2):
+        # return angle
+        return
+    
+    def add(self, v1, v2):
+        # return vector
+        return
+    
+    def subtract(self, v1, v2):
+        # return vector
+        return
+    
+    def normalize(self, v):
+        # return normalized vector
+        return
+    
+    # with and without squareroot
+    def magnitude(self, v, sqrt):
+        # return magnitude of vector
+        return
+    
+    def eq(self, v1, v2):
+        # return boolean if vectors have same values
+        return
